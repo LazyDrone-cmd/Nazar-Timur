d2e6354d47623b63d8f07f4371fd7e9c0f26b8d7
LazyDrone-cmd <nazarlashko11@gmail.com>
2026-05-11 18:06:33 +0200

    Project_1.0
    Добавил класс для управления,
    список в котором будут храниться задачи

--- Program.cs
+++ Program.cs
@@ -5,17 +5,41 @@ public enum TaskStatus
     InProgress,
     Done
 }
+
+
+
 class Program
 {
-    
+    static List<TaskItem> tasks = new List<TaskItem>();
     class TaskItem
     {
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public TaskStatus Status { get; set; }
+    }
+    class TaskManager 
+    {
+        public void AddTask()
+        {
+            
+        }
+        public void DeleteTask()
+        {
 
+        }
+        public void getAllTasks()
+        {
 
+        }
+        public void getIdTask()
+        {
+
+        }
+        public void ChangeStatus()
+        {
+
+        }
     }
 
     static void Main()
