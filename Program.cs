acdf3f9dbafcedca8d8d1593497f8ab8b82a12a7
LazyDrone-cmd <nazarlashko11@gmail.com>
2026-05-11 17:50:45 +0200

    Create Program.cs
    Структура

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+public enum TaskStatus
+{
+    New,
+    InProgress,
+    Done
+}
+class Program
+{
+    
+    class TaskItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public TaskStatus Status { get; set; }
+
+
+    }
+
+    static void Main()
+    {
+
+    }
+}
